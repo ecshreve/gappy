86880e625056af35ef24407cfddf71e454c26609
Eric Shreve <ecshreve13@gmail.com>
2021-01-12 22:10:22 -0800

    print empty game

--- cmd/gappy/main.go
+++ cmd/gappy/main.go
@@ -5,5 +5,5 @@ import (
 )
 
 func main() {
-	game.DoSomethig()
+	game.RunTheGame()
 }
--- internal/game/game.go
+++ internal/game/game.go
@@ -2,6 +2,33 @@ package game
 
 import "fmt"
 
-func DoSomethig() {
-	fmt.Println("hi")
+func RunTheGame() {
+	PrintInitialGame()
+}
+
+const (
+	GameW = 60
+	GameH = 12
+)
+
+func PrintInitialGame() {
+	fmt.Print("+")
+	for i := 0; i < GameW; i++ {
+		fmt.Print("-")
+	}
+	fmt.Println("+")
+
+	for i := 0; i < GameH; i++ {
+		fmt.Print("|")
+		for j := 0; j < GameW; j++ {
+			fmt.Print(" ")
+		}
+		fmt.Println("|")
+	}
+
+	fmt.Print("+")
+	for i := 0; i < GameW; i++ {
+		fmt.Print("-")
+	}
+	fmt.Println("+")
 }
